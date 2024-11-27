f661157c27161753331887f7e20ad24136bfd1c2
lithium03 <contact@lithium03.info>
2024-11-27 15:05:04 +0900

    fix loss

--- loss_func.py
+++ loss_func.py
@@ -128,7 +128,7 @@ def loss_function(fmask, labelmap, idmap, heatmap, decoder_outputs):
     for i in range(4):
         label_map = ((idmap[:,1,:,:] & 2**(i)) > 0).to(torch.float32)
         predict_map = heatmap[:,5+i,:,:]
-        weight = torch.ones_like(label_map) + label_map * weight2 * 2 + weight2 * 4
+        weight = torch.ones_like(label_map) + label_map * weight2 * 1 + weight2 * 3
         code_loss = torch.nn.functional.binary_cross_entropy_with_logits(predict_map, label_map, weight=weight)
         code_losses['code%d_loss'%2**(i)] = code_loss
     
