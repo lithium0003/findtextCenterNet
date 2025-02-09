a5a5feb60cafbba3f7771b10727c0e412afd9db5
lithium03 <contact@lithium03.info>
2025-02-09 09:10:56 +0900

    fix

--- train3.py
+++ train3.py
@@ -181,6 +181,7 @@ def train():
 
             loss, rawloss = train_step(feature, codes[:,:-1], codes[:,1:], smoothing)
             loss.backward()
+            torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
             optimizer.step()
             # scaler.scale(loss).backward()
             # scaler.step(optimizer)
