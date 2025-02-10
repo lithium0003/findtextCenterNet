40ca52c6d9f53dbf236cff827cddfedf0ffadf3e
lithium03 <contact@lithium03.info>
2025-02-10 17:07:59 +0900

    fix

--- models/transformer.py
+++ models/transformer.py
@@ -303,7 +303,6 @@ class Transformer(nn.Module):
 
     def forward(self, enc_input, dec_input):
         key_mask = torch.all(enc_input == 0, dim=-1)
-        key_mask = torch.logical_and(key_mask, torch.any(torch.logical_not(key_mask), dim=-1).unsqueeze(1))
         key_mask = torch.where(key_mask[:,None,None,:], float("-inf"), 0).expand(-1,-1,self.max_len,-1)
         enc_output = self.encoder(enc_input, key_mask=key_mask)
         output = self.decoder(dec_input, enc_output, causal_mask=self.causal_mask, key_mask=key_mask)
