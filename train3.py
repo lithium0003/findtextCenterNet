1bd1b3eedb08ff166fd03023522f2ea28146bd97
lithium03 <contact@lithium03.info>
2025-02-12 04:41:25 +0900

    fix

--- train3.py
+++ train3.py
@@ -247,7 +247,7 @@ def train():
         if 0 < scheduler_gamma < 1.0:
             scheduler.step() 
 
-        if loss_down == 0 and loss_value < 1:
+        if loss_down == 0 and loss_value < 0.5:
             loss_down += 1
             for group in optimizer.param_groups:
                 group['lr'] /= 10
