f9ccc699828806a374d4eae69ec259ad9dc58d41
lithium03 <contact@lithium03.info>
2025-05-23 21:56:31 +0900

    fix model

--- models/transformer.py
+++ models/transformer.py
@@ -296,7 +296,7 @@ class Transformer(nn.Module):
 class ModelDimensions:
     enc_input_dim: int = encoder_dim
     embed_dim: int = 512
-    head_num: int = 32
+    head_num: int = 8
     enc_block_num: int = 4
     dec_block_num: int = 4
     max_enc_seq_len: int = max_encoderlen
