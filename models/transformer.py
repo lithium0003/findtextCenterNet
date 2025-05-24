af47f0e024356a2145d4e14a7c4d3848fb14d77b
lithium03 <contact@lithium03.info>
2025-05-24 23:26:22 +0900

    fix model

--- models/transformer.py
+++ models/transformer.py
@@ -297,7 +297,7 @@ class ModelDimensions:
     embed_dim: int = 512
     head_num: int = 16
     enc_block_num: int = 0
-    dec_block_num: int = 8
+    dec_block_num: int = 12
     max_enc_seq_len: int = max_encoderlen
     max_dec_seq_len: int = max_decoderlen
     dropout: float = 0.0
