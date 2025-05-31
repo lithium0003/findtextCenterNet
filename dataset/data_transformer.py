616357808ce59e47b4c49a75edca29281f7576a8
lithium03 <contact@lithium03.info>
2025-06-01 00:19:36 +0900

    fix dataset

--- dataset/data_transformer.py
+++ dataset/data_transformer.py
@@ -505,7 +505,11 @@ class TransformerDataDataset(torch.utils.data.Dataset):
         return self.pad_output(txt, feat)
 
     def add_noise(self, value):
-        return value * (1 + 0.5 * rng.normal(loc=0, scale=1, size=value.shape)) + 5 * rng.normal(loc=0, scale=1, size=value.shape)
+        pls_noise = 5 * rng.normal(loc=0, scale=1, size=value.shape)
+        mul_noise = 0.5 * rng.normal(loc=0, scale=1, size=value.shape)
+        pls_noise[...,feature_dim:] = 0
+        mul_noise[...,feature_dim:] = 0
+        return value * (1 + mul_noise) + pls_noise
 
     def generage_feature(self, code, horizontal):
         hori, vert = self.charparam.get(code, (None, None))
