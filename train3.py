2029c4c8cdf0873207b2f2b9e5569e8ec346ef5e
lithium03 <contact@lithium03.info>
2025-06-04 06:24:58 +0900

    fix

--- train3.py
+++ train3.py
@@ -16,7 +16,7 @@ from dataset.data_transformer import TransformerDataDataset
 from loss_func import loss_function3
 from const import decoder_PAD, decoder_SOT, decoder_EOT, decoder_MSK
 
-EPOCHS = 60
+EPOCHS = 50
 lr=1e-3
 batch=256
 logstep=10
@@ -120,7 +120,7 @@ def train():
 
     all_params = list(filter(lambda p: p.requires_grad, model.parameters()))
     optimizer = RAdamScheduleFree(all_params, lr=lr)
-    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=0, factor=0.5, min_lr=5e-5)
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=0, factor=0.5, min_lr=1e-5)
 
     running_loss = RunningLoss(device=device, runningcount=100, losses=[
         'loss',
@@ -149,7 +149,7 @@ def train():
 
     scaler = torch.amp.GradScaler()
     for epoch in range(last_epoch, EPOCHS):
-        training_dataset.noise_ratio = 0.98 ** epoch
+        training_dataset.noise_ratio = 0.97 ** epoch
 
         print(datetime.datetime.now(), 'epoch', epoch, flush=True)
         print(datetime.datetime.now(), 'lr', optimizer.param_groups[0]['lr'], flush=True)
